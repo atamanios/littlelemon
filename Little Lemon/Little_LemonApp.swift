53d0cc2a0d9a109f9a74e396168502e02ee69ec7
atamanios <atamandeniz@hotmail.com>
2023-02-13 12:24:09 +0400

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Ataman Deniz on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
